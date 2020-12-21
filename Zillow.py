b0c55584992b66eb3416174156c25261f80fc276
Leon <lshen1905@gmail.com>
2020-12-20 21:54:09 -0700

    Filled in some helper functions

--- Zillow.py
+++ Zillow.py
@@ -7,32 +7,39 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import re
 import random
+import requests
+import json
+from urllib.request import urlopen
 class Property_Grab:
     def __init__(self,):
         # self.set_up_proxy()
-        self.driver = webdriver.Chrome()
-        opt = Options()
-        ua = UserAgent()
-        userAgent = ua.random
-        self.set_viewport_size(800,600)
-        opt.add_argument(f"user-agent={userAgent}")
-        opt.add_argument('--disable-blink-features=AutomationControlled')
-        self.properties = []
+        self.zipcodes = {}
 
     def read_file(self,zipcode):
         f = open("zipcodes.txt")
+        count = 1 # Skips the first line
         for x in f:
-            #TODO Fill in Regex
-            file_zip = re.match("")
-            if file_zip == zipcode:
-                #TODO extract ID from text file
-                ID = ""
-                return ID
+            if count != 1:
+                file_zip = x.split()
+                if file_zip[0] == zipcode:
+                    self.ID = file_zip[1]
+                    print(self.ID)
+                    f.close()
+                    return True
+            else:
+                count += 1
+        print("Zipcode not found")
+        f.close()
         return False
 
-    def create_link(self, ID):
-        #Fill in ID
-        url = "" + ID
+    def zipcodes_append(self, key, value):
+        self.zipcodes[key] = value
+
+    def zipcodes_get(self, key):
+        return self.zipcodes[key]
+
+    def create_link(self):
+        url = "https://www.zillow.com/homes/" + self.ID
         return url
 
     def parse_json(self):
@@ -44,7 +51,26 @@ class Property_Grab:
 
     def locate_id(self,zipcode):
         #TODO Go onto zillow and find the unique id for the zipcode
-        url = "" + zipcode
+        url = "https://www.zillow.com/homes/" + zipcode+"_rb"
+        headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.11 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+                   'Accept-Encoding': 'identity'
+                   }
+        response = requests.get(url,headers=headers)
+        new_url = urlopen(url).read()
+        soup = BeautifulSoup(new_url,'lxml')
+        data = soup.find_all('script',type = 'application/json')
+
+        for x in data:
+            print(x)
+                # if is_json_key_present(new_json,'mapBounds'):
+                #     print(new_json+"\n-------------")
+        # print(soup.prettify())
+        # self.driver = webdriver.Chrome()
+        # opt = Options()
+        # ua = UserAgent()
+        # userAgent = ua.random
+        # opt.add_argument(f"user-agent={userAgent}")
+        # opt.add_argument('--disable-blink-features=AutomationControlled')
 
 
 
@@ -52,4 +78,6 @@ class Property_Grab:
 
 zipcode = input("What zipcode do you want to look up?: ")
 test = Property_Grab()
+test.locate_id(zipcode)
+
 
